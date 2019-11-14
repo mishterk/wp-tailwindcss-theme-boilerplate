8292d1dea76555385de4754142ccbafdc8eb8299
Phil Kurth <phil@philkurth.com.au>
2019-11-15 08:16:42 +1100

    Modify example partial to use Tailwind

--- templates/partials/example.php
+++ templates/partials/example.php
@@ -1,26 +1,19 @@
-<div class="Example">
-    <div class="Example__inner">
+<div class="Example bg-blue-100 fixed flex inset-0 items-center justify-center">
+    <div class="Example__inner bg-white max-w-2xl p-12 rounded-lg shadow-lg text-center">
 
-        <h1 class="Example__title">WP Laravel Mix Theme Boilerplate</h1>
+        <h1 class="Example__title">WP Tailwind CSS Boilerplate</h1>
 
-        <p>This is a just a very minimalistic theme boiler that gets you started with Laravel Mix for building your
-            project assets.</p>
-
-        <h2>Why use Laravel Mix?</h2>
-
-        <p>Simply put, it is a very straight-forward and easy to use wrapper for webpack that removes the need for
-            a lot of the configuration that we would normally have to go through when using alternatives.</p>
-
-        <p>For a sense of how simple it is to set up, check out the webpack.mix.js file in the them root.
-            <a href="https://laravel.com/docs/5.8/mix" target="_blank">You can learn more about Laravel Mix here.</a>
-        </p>
+        <p>This is a just a very minimalistic theme boiler that gets you started with
+            <a href="https://tailwindcss.com/" target="_blank" rel="noopener noreferrer">Tailwind CSS</a> &
+            <a href="https://laravel.com/docs/5.8/mix" target="_blank" rel="noopener noreferrer">Laravel Mix</a> for
+            building custom WordPress themes.</p>
 
 		<?php
 		// You may use 'views' to render your UI components. Views are designed for use anywhere as they, ideally, have
 		// no context-specific functionality – the simply render data handed to them as follows:
 		\WPLMixTheme\View::render( 'ExampleView', [
 			'title' => 'Who am I?',
-			'text' => 'My name is Phil Kurth and I built themes and plugins for WordPress. You can learn more about me at <a href="https://philkurth.com.au/" target="_blank">philkurth.com.au</a>',
+			'text' => 'My name is Phil Kurth and I build themes and plugins for WordPress. You can learn more about me at <a href="https://philkurth.com.au/" target="_blank">philkurth.com.au</a>',
 		] ); ?>
 
     </div>
