1ac252a49ed985750a6eedb0a8c57051e8e861e7
Phil Kurth <phil@philkurth.com.au>
2019-11-14 18:34:35 +1100

    Tailwind CSS and Laravel Mix installed and set up

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,9 @@
 const mix = require('laravel-mix');
 const local = require('./local');
+const glob = require('glob-all');
 require('laravel-mix-versionhash');
+require('laravel-mix-tailwind');
+require('laravel-mix-purgecss');
 
 mix.setPublicPath('./build');
 
@@ -21,8 +24,20 @@ if (local.proxy) {
     });
 }
 
+mix.tailwind();
+
 mix.js('assets/js/app.js', 'js');
 mix.sass('assets/scss/app.scss', 'css');
+
+// The package internally limits running to production builds so we don't need
+// to wrap this up with a condition.
+mix.purgeCss({
+    paths: () => glob.sync([
+        path.join(__dirname, '*.php'),
+        path.join(__dirname, 'templates/**/*.php'),
+        path.join(__dirname, 'build/js/**/*.js'),
+    ]),
+});
 
 if (mix.inProduction()) {
     mix.versionHash();
