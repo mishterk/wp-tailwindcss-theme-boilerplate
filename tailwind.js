383cb2f4f0d202e00958ce873268a4a16b6de21f
Phil Kurth <phil@philkurth.com.au>
2019-11-30 07:55:16 +1100

    Add some base config

--- tailwind.js
+++ tailwind.js
@@ -1,12 +1,34 @@
 module.exports = {
     theme: {
+        container: {
+            padding: '1.5rem',
+        },
         extend: {
             colors: {
                 link: {
                     'default': '#3182ce',
                     'hover': '#63b3ed',
                 }
-            }
+            },
+            fontSize: {
+                xxs: '0.675rem',
+            },
+            lineHeight: {
+                tighter: '1.125',
+            },
         }
-    }
+    },
+    variants: {
+        textColor: ['responsive', 'hover', 'focus', 'visited'],
+    },
+    plugins: [
+        ({addUtilities}) => {
+            const utils = {
+                '.translate-x-half': {
+                    transform: 'translateX(50%)',
+                },
+            };
+            addUtilities(utils, ['responsive'])
+        }
+    ]
 };
