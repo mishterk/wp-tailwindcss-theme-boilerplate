22dab85e87dc05e867f471b9b3725c7b4fda94d9
Phil Kurth <phil@philkurth.com.au>
2021-05-21 17:25:14 +1000

    auto format code - no changes

--- templates/partials/example.php
+++ templates/partials/example.php
@@ -1,12 +1,12 @@
 <div class="Example bg-blue-100 fixed flex inset-0 items-center justify-center">
-    <div class="Example__inner bg-white max-w-2xl p-12 rounded-lg shadow-lg text-center">
+	<div class="Example__inner bg-white max-w-2xl p-12 rounded-lg shadow-lg text-center">
 
-        <h1 class="Example__title">WP Tailwind CSS Theme Boilerplate</h1>
+		<h1 class="Example__title">WP Tailwind CSS Theme Boilerplate</h1>
 
-        <p class="text-gray-700">This is a just a very minimalistic theme boiler that gets you started with
-            <a href="https://tailwindcss.com/" target="_blank" rel="noopener noreferrer">Tailwind CSS</a> &
-            <a href="https://laravel.com/docs/5.8/mix" target="_blank" rel="noopener noreferrer">Laravel Mix</a> for
-            building custom WordPress themes.</p>
+		<p class="text-gray-700">This is a just a very minimalistic theme boiler that gets you started with
+			<a href="https://tailwindcss.com/" target="_blank" rel="noopener noreferrer">Tailwind CSS</a> &
+			<a href="https://laravel.com/docs/5.8/mix" target="_blank" rel="noopener noreferrer">Laravel Mix</a> for
+			building custom WordPress themes.</p>
 
 		<?php
 		// You may use 'views' to render your UI components. Views are designed for use anywhere as they, ideally, have
@@ -16,5 +16,5 @@
 			'text' => 'My name is Phil Kurth and I build themes and plugins for WordPress. You can learn more about me at <a href="https://philkurth.com.au/" target="_blank">philkurth.com.au</a>',
 		] ); ?>
 
-    </div>
+	</div>
 </div>
--- templates/views/ExampleView.php
+++ templates/views/ExampleView.php
@@ -8,11 +8,11 @@ $text = isset( $data->text ) ? $data->text : "";
 <div class="ExampleView border-t border-gray-200 <?= $class ?>" id="<?= $id ?>">
 
 	<?php if ( $title ): ?>
-        <h2 class="pt-8 text-2xl"><?= $title ?></h2>
+		<h2 class="pt-8 text-2xl"><?= $title ?></h2>
 	<?php endif; ?>
 
 	<?php if ( $text ): ?>
-        <p class="text-gray-700"><?= $text ?></p>
+		<p class="text-gray-700"><?= $text ?></p>
 	<?php endif; ?>
 
 </div>
